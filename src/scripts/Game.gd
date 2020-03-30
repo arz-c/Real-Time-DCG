a5e3fe2fd5fe0dc511e8b861c17911bf3f9c2ea0
ShxdowRaven <shxdowreaper@gmail.com>
2020-03-30 15:37:00 -0400

    Updated card positions based on window size

--- src/scripts/Game.gd
+++ src/scripts/Game.gd
@@ -1,23 +1,29 @@
 extends Node2D
 
+const UI_MARGIN = 150
+
 var deck1: Deck
 var deck2: Deck
 var hand1: Hand
 var hand2: Hand
 
+# TODO add left/right to hands
+
 func _ready() -> void:
+	var window_size = get_viewport().get_size()
+	
 	# Decks
-	deck1 = Deck.new(Vector2(1575, 150), 30, 180)
+	deck1 = Deck.new(Vector2(window_size.x - UI_MARGIN, UI_MARGIN), 30, 180)
 	add_child(deck1)
 	
-	deck2 = Deck.new(Vector2(100, 810), 30, 0)
+	deck2 = Deck.new(Vector2(UI_MARGIN, window_size.y - UI_MARGIN), 30, 0)
 	add_child(deck2)
 	
 	# Hands
-	hand1 = Hand.new(Vector2(800, 150), 180)
+	hand1 = Hand.new(Vector2(window_size.x / 2, UI_MARGIN), 180)
 	add_child(hand1)
 	hand1.take_from_deck(deck1, 5)
 	
-	hand2 = Hand.new(Vector2(800, 810), 0)
+	hand2 = Hand.new(Vector2(window_size.x / 2, window_size.y - UI_MARGIN), 0)
 	add_child(hand2)
 	hand2.take_from_deck(deck2, 5)
